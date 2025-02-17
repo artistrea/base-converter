7db1fc4214cdddbc806d3383a4a744674d7d1aa1
artistrea <apiravesi@gmail.com>
2025-02-17 11:51:38 -0300

    feat: example done in class

--- main.c
+++ main.c
@@ -35,10 +35,6 @@ void encode(FILE* inptr, FILE* outptr) {
 
     // returns encoded in big endian order
     uint32_t encoded = base64_encode(n_read, text);
-    // // char* c = (char*)&encoded;
-    // printf("nread: %d\n", n_read);
-    // printf("in: %d, %d, %d\n", value[0], value[1], value[2]);
-    // printf("inn: %016b\n", encoded);
 
     if (is_little_endian_arch) {
       encoded = ((encoded >> 24) & 0xff) |      // move byte 3 to byte 0
@@ -46,7 +42,6 @@ void encode(FILE* inptr, FILE* outptr) {
                 ((encoded >> 8) & 0xff00) |     // move byte 2 to byte 1
                 ((encoded << 24) & 0xff000000); // byte 0 to byte 3
     }
-    // printf("out: %c, %c, %c, %c\n", ((uint8_t*)&encoded)[0], ((uint8_t*)&encoded)[1], ((uint8_t*)&encoded)[2], ((uint8_t*)&encoded)[3]);
 
     fwrite(&encoded, sizeof(char), 4, outptr);
 #ifdef NEWLINE_SPACING
@@ -83,10 +78,6 @@ void decode(FILE* inptr, FILE* outptr) {
 
     // returns encoded in big endian order
     uint32_t encoded = base64_decode(text);
-    // // char* c = (char*)&encoded;
-    // printf("nread: %d\n", n_read);
-    // printf("in: %d, %d, %d\n", value[0], value[1], value[2]);
-    // printf("inn: %016b\n", encoded);
 
     if (is_little_endian_arch) {
       encoded = ((encoded >> 24) & 0xff) |      // move byte 3 to byte 0
@@ -94,7 +85,6 @@ void decode(FILE* inptr, FILE* outptr) {
                 ((encoded >> 8) & 0xff00) |     // move byte 2 to byte 1
                 ((encoded << 24) & 0xff000000); // byte 0 to byte 3
     }
-    // printf("out: %c, %c, %c, %c\n", ((char*)&encoded)[0], ((char*)&encoded)[1], ((char*)&encoded)[2], ((char*)&encoded)[3]);
 
     if (value[2] == '=') {
       fwrite(&encoded, sizeof(char), 1, outptr);
